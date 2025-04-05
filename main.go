809fd2eec76255266ec60978d19ab1bc0f0c1d28
Xavanion <barrettbrown2012@gmail.com>
2025-04-05 12:07:10 -0500

    Go comments

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"net/http"
+
+	roomhandling "github.com/Xavanion/Hack-KU-2025/backend/room-handling"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"github.com/Xavanion/Hack-KU-2025/backend/room-handling"
 )
 
+// Upgrade HTTP request to WS connection
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
@@ -15,7 +17,6 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
-
 
 func main() {
 	router := gin.Default()
@@ -26,10 +27,12 @@ func main() {
 	// Setup route group for the API
 	api := router.Group("/api")
 	{
+		// API pong response
 		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{"message": "pong",})
+			c.JSON(http.StatusOK, gin.H{"message": "pong"})
 		})
 
+		// Websocket endpoint
 		api.GET("/ws", func(c *gin.Context) {
 			// Upgrade GET request to a WebSocket
 			conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
@@ -38,6 +41,7 @@ func main() {
 			}
 			defer conn.Close()
 
+			// Wait and read incoming messages
 			for {
 				// Read message
 				messageType, message, err := conn.ReadMessage()
