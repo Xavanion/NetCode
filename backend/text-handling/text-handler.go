8aceb1debac25cd14c820240bf2f16017f1121a8
AlexanderPhibbs <alex.r.phibbs@gmail.com>
2025-04-05 16:34:35 -0500

    fixed dumb spacing all over

--- backend/text-handling/text-handler.go
+++ backend/text-handling/text-handler.go
@@ -1,26 +1,27 @@
 package texthandler
+
 import (
 	"github.com/deadpixi/rope"
 )
-var main_rope rope.Rope 
 
-func Initialize(){
+var main_rope rope.Rope
+
+func Initialize() {
 	main_rope = rope.New()
 }
 
-func Insert_text(position int, text string){
+func Insert_text(position int, text string) {
 	main_rope.InsertString(position, text)
 }
 
-func Append_text(text string){
+func Append_text(text string) {
 	main_rope.AppendString(text)
 }
 
-func Length() int{
+func Length() int {
 	return main_rope.Length()
 }
 
-
-func Test() int{
+func Test() int {
 	return 0
 }
