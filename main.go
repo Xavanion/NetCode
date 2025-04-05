f687580b2e198f34641a36feaf89e19d660b96ff
Anorak2 <adamcoleberry@gmail.com>
2025-04-04 22:35:54 -0500

    fixed the routing in main

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -17,44 +16,31 @@ type User struct {
 var users []User
 
 func handleHTTP(w http.ResponseWriter, r *http.Request) {
-	// Telling the program the path to all relevant files
-
 	// extract only the relevant part for us
-	fmt.Println(r.URL.Path)
 	file_name := r.URL.Path[len("/"):]
-	fmt.Println(file_name)
-	filepath := "Frontend/" 
-	serveFile(w, r, filepath, file_name)
+	filepath := "Frontend/" + file_name 
+	serveFile(w, r, filepath)
 }
 
-func serveFile(w http.ResponseWriter, r *http.Request, filepath string, file string) {
-	//filepath := path.Clean(FilePath) // clean the file path from things like ..
-	extension := r.Header.Get("Accept")
-	fmt.Print(extension)
-	//extension := path.Ext(filepath) // see if there is a file extension
+func serveFile(w http.ResponseWriter, r *http.Request, filepath string) {
+
+	// clean the file path from things like ..
+	filepath = path.Clean(filepath) 
+	extension := path.Ext(filepath) // see if there is a file extension
 	switch extension {
-		case "application/json":
-			filepath += "json/" + file
+		case ".js":
 			w.Header().Set("Content-Type", "application/javascript")
-		case "text/css":
-			filepath += "css/" + file
+		case ".css":
 			w.Header().Set("Content-Type", "text/css")
 		default:
-			if(file == ""){
-				file = "index"	
-			}
-			filepath += "html/" + file + ".html"
+			filepath += ".html"
 			w.Header().Set("Content-Type", "text/html")
 		}
-	// clean the file path from things like ".." to prevent escapes
-	filepath = path.Clean(filepath) 
-	fmt.Print(filepath)
 	// Check if the file exists in our system
     if _, err := os.Stat(filepath); os.IsNotExist(err) {
         http.NotFound(w, r)
         return
     }
-	fmt.Print(filepath)
 	// actually serve the file
 	http.ServeFile(w, r, filepath)
 }
@@ -62,12 +48,13 @@ func serveFile(w http.ResponseWriter, r *http.Request, filepath string, file str
 
 func main() {
 	fmt.Println("test")
-	router := mux.NewRouter()
 	//router.HandleFunc("/users", getUsers).Methods("GET")
 	//router.HandleFunc("/users", createUser).Methods("POST")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     switch r.URL.Path {
-		case "/", "/index", "/home.js":
+		case "/":
+			handleHTTP(w, r)
+		case "/index", "/home.js":
 			handleHTTP(w, r);
 		case "/about", "/info":
 			break
@@ -76,7 +63,7 @@ func main() {
     }
 })
 
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(":8080", nil)
 	if(err != nil){
 		fmt.Println("Error starting server:",err)
 	}
