d2b8d43d2762a2645ac59e5c67ae778d1f38b865
Anorak2 <adamcoleberry@gmail.com>
2025-04-04 22:56:51 -0500

    added a default path to index.html

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
+func handleHome(w http.ResponseWriter, req *http.Request){
+	http.ServeFile(w, req, "real-time-app/index.html");
+}
+
+
 func main() {
 	fmt.Println("test")
 	fs := http.FileServer(http.Dir("real-time-app/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
+	http.HandleFunc("/", handleHome)
 	err := http.ListenAndServe(":8080", nil)
 	if(err != nil){
 		fmt.Println("Error starting server:",err)
