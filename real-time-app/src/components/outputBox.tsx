e22f15b85e9a3eebf7f427889cf2d474bc086912
Xavanion <barrettbrown2012@gmail.com>
2025-04-06 07:26:28 -0500

    Markdown

--- real-time-app/src/components/outputBox.tsx
+++ real-time-app/src/components/outputBox.tsx
@@ -1,20 +1,26 @@
 import '../styles/outputBox.css';
+import ReactMarkdown from 'react-markdown';
 
 type Props = {
   curText: string;
   activeOutput: 'terminal' | 'review';
 };
 
-function Outputbox({ curText, activeOutput,  }: Props) {
+function Outputbox({ curText, activeOutput }: Props) {
   return (
     <div className="outputbox-container">
-
-      <textarea
-        readOnly
-        value={curText}
-        placeholder={activeOutput === 'terminal' ? 'Output will be shown here' : 'Code review will be shown here'}
-        className="output_textarea"
-      />
+      {activeOutput === 'terminal' ? (
+        <textarea
+          readOnly
+          value={curText}
+          placeholder="Output will be shown here"
+          className="output_textarea"
+        />
+      ) : (
+        <div className="output_markdown">
+          <ReactMarkdown>{curText}</ReactMarkdown>
+        </div>
+      )}
     </div>
   );
 }
