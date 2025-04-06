01b97ecc846bcf957fc39a97c9fe8b95b91257de
Xavanion <barrettbrown2012@gmail.com>
2025-04-06 01:25:47 -0500

    fixed buildingg

--- real-time-app/src/components/outputBox.tsx
+++ real-time-app/src/components/outputBox.tsx
@@ -1,20 +1,11 @@
 import '../styles/outputBox.css';
-import { useRopes } from '../hooks/useRopes';
-import { useEffect, useState } from 'react';
 
 function Outputbox() {
-  const [outputText] = useRopes();
-  const [output, setOutput] = useState(null);
-
-  useEffect(()=> {
-    setOutput(outputText);
-  }, [outputText])
   return (
     <div className="outputboxContainer">
       <textarea
         readOnly
         placeholder="Output will be shown here"
-        value={output}
         className="output_textarea"
       />
     </div>
