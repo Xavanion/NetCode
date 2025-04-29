571075735bb7777d99f503c0b837604fe7f0cbf9
Xavanion <barrettbrown2012@gmail.com>
2025-04-28 23:32:23 -0500

    Removed bootstrap and adjusting landing page dimensions

--- real-time-app/src/pages/Landing.tsx
+++ real-time-app/src/pages/Landing.tsx
@@ -1,11 +1,12 @@
 function Landing() {
     return (
-      <>
-        <h1 className="text-5xl text-blue-500 font-bold text-center mt-10">
-          Welcome to NetCode!
-        </h1>
-      </>
-    );
-  }
+        <section className="min-h-screen bg-red-50 px-10 py-10">
+            <h1 className="text-5xl font-bold">
+                Welcome to NetCode!
+            </h1>
+        </section>
+);
+}
+  
   
 export default Landing;
